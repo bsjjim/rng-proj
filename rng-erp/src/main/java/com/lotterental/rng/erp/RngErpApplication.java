da5bb65b74509a9628c5d919a4064db7c8b4b898
Byun Sang June <bsjjim@nate.com>
2022-05-02 11:02:55 +0900

    erp 소스 추가

---
+++ rng-erp/src/main/java/com/lotterental/rng/erp/RngErpApplication.java
@@ -0,0 +1,13 @@
+package com.lotterental.rng.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RngErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RngErpApplication.class, args);
+    }
+
+}
